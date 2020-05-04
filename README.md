c123749028ea5a5f9a2fb60362fc049a02d8924a
MukundChourey <mukundchourey@gmail.com>
2020-05-04 10:51:11 +0530

    database connected

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Basic-Ajax-Use
 This is just a sample set of codes for the basic understanding of Ajax using js, jQuery , php.
+Make the necessary database.
---
+++ insertphp.php
@@ -0,0 +1,14 @@
+<?php
+$con = mysqli_connect('localhost','root');
+mysqli_select_db('$con',test);
+
+extract($_POST);
+
+if(isset($_POST['submit'])){
+
+  $q = "INSERT INTO user_details (name,passswd) VALUES ('$username','$password')";
+
+}
+
+
+ ?>
